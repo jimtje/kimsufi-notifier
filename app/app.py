6c9c1d5a3a0891dd783cdac6242471990904cdd2
Paolo Basso <basso.paolo99@gmail.com>
2020-01-27 16:02:32 +0100

    Add notifications

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+import time
 
 from dotenv import load_dotenv
 from WantedModels import WantedModels
@@ -9,7 +10,6 @@ from WantedModels import WantedModels
 def run():
     load_dotenv()
 
-    WANTED_REGION = os.getenv('WANTED_REGION')
     wanted_models = WantedModels(os.getenv('WANTED_MODELS'))
 
     # Get Data
@@ -20,8 +20,8 @@ def run():
     aviable_models = {}
     for model in models:
         hardware_code = model["hardware"][-4:]
-        
-        if model["region"] == WANTED_REGION and wanted_models.has_hardware(hardware_code):
+
+        if model["region"] == os.getenv('WANTED_REGION') and wanted_models.has_hardware(hardware_code):
             aviable = False
             for datacenter in model["datacenters"]:
                 if (not datacenter["availability"] == 'unavailable'):
@@ -31,10 +31,79 @@ def run():
             if (aviable and not hardware_code in aviable_models.values()):
                 key = wanted_models.get_key_by_hardware_code(hardware_code)
                 aviable_models[key] = model["hardware"]
-    
+
     # Notify
     if (len(aviable_models) > 0):
-        print("Notify")
+        NOTIFICATIONS_URL = os.getenv('NOTIFICATIONS_URL')
+        EMAIL_FROM = os.getenv('EMAIL_FROM')
+        KIMSUFI_BUY_URL = os.getenv('KIMSUFI_BUY_URL')
+
+        headers = {
+            'user-agent': 'kimsufi-notifier',
+            'Content-Type': 'application/json',
+            'X-Api-Key': os.getenv('NOTIFICATIONS_API_KEY')
+        }
+
+        # Get handlers from env
+        handlers = os.getenv('NOTIFICATIONS_HANDLERS').split(',')
+        handlers = map(lambda handler: handler.lower().strip(), handlers)
+        handlers = list(handlers)
+
+        # Send notification
+        buffers_file = 'buffers.json'
+        for key, hardware in aviable_models.items():
+            if (is_to_notify(buffers_file, key)):
+                payload = {
+                    "handlers": handlers,
+                    "emailFrom": EMAIL_FROM,
+                    "subject": f"Kimsufi {key} Aviable!",
+                    "message": f"The Kimsufi {key} server is aviable, grab it here: {KIMSUFI_BUY_URL}?reference={hardware}"
+                }
+
+                res = requests.post(
+                    NOTIFICATIONS_URL,
+                    headers=headers,
+                    data=json.dumps(payload)
+                )
+
+                # If one notification succeeded save time to file
+                res_data = res.json()
+                if (res_data['oneSucceeded']):
+                    save_notification_time(buffers_file, key)
+
+
+def save_notification_time(buffers_file, key):
+    with open(buffers_file, 'r+', encoding='utf-8') as json_file:
+        # Load buffer dictionary
+        buffers = {}
+        if (len(json_file.read()) > 0):
+            json_file.seek(0)
+            buffers = json.load(json_file)
+
+        # Save time
+        buffers[key] = int(time.time())
+        json_file.seek(0)
+        json.dump(buffers, json_file, ensure_ascii=False, indent=4)
+
+
+def is_to_notify(buffers_file, key):
+    # Check if file exists
+    if (not os.path.isfile(buffers_file)):
+        open(buffers_file, 'a').close()
+        return True
+
+    # Read
+    with open(buffers_file, 'r+', encoding='utf-8') as json_file:
+        json_file.seek(0)
+        buffers = json.load(json_file)
+
+        if(not key in buffers):
+            return True
+        else:
+            buffer_minutes = int(os.getenv('NOTIFICATION_BUFFER'))
+            maxTime = time.time() - buffer_minutes*60
+
+            return buffers[key] < maxTime
 
 
 if __name__ == '__main__':
